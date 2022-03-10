e06a19e61ef9eed35fcf801b885ef5d2075d8850
jasmineblinks <puenehfaith@gmail.com>
2022-03-10 01:36:17 +0100

    updated the javascript

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-// const messageInput = document.getElementById("message-input");
-// messageInput.addEventListener("keydown", function (event) {
-//   if (event.key === "Enter") {
-//     getMessage();
-//   }
-// });
-// function getMessage() {
-//   document.getElementById("message-output").innerHTML = messageInput.value;
-//   messageInput.value = "";
-// }
-// getMessage();
+const messageInput = document.getElementById("message-input");
+messageInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    getMessage();
+  }
+});
+function getMessage() {
+  document.getElementById("message-output").innerHTML = messageInput.value;
+  messageInput.value = "";
+}
+getMessage();
